da2e3443dd7bed686333c0d4ec483655e91bc278
Hiro-lowskill <81725906+Hiro-lowskill@users.noreply.github.com>
2021-04-02 06:44:43 +0300

    update fixed bug

--- app/src/main/java/com/example/readbull/conkyrs.java
+++ app/src/main/java/com/example/readbull/conkyrs.java
@@ -2,7 +2,9 @@ package com.example.readbull;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class conkyrs extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class conkyrs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conkyrs);
+    }
+    public void btn1(View view) {
+        Intent i = new Intent(this, panel.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/readbull/me.java
+++ app/src/main/java/com/example/readbull/me.java
@@ -3,7 +3,9 @@ package com.example.readbull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 @SuppressLint("Registered")
 public class me extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class me extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me);
+    }
+    public void btn1(View view) {
+        Intent i = new Intent(this, panel.class);
+        startActivity(i);
+        finish();
     }
 }
